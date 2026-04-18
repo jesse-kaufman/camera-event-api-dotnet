a8945f4f9ebe80f4c47715fcf6ec92fd148fcf64
jesse-kaufman <jesse@jessekaufman.com>
2026-04-18 13:59:31 -0500

    docs: add comments for quicker scanning

--- Data/Entities/CameraEventConfiguration.cs
+++ Data/Entities/CameraEventConfiguration.cs
@@ -8,18 +8,26 @@ public class CameraEventConfiguration : IEntityTypeConfiguration<CameraEvent>
   public void Configure(EntityTypeBuilder<CameraEvent> builder)
   {
     builder.ToTable("CameraEvents");
+
+    // Id
     builder.HasKey(e => e.Id);
 
+    // Timestamp
     builder.Property(e => e.Timestamp).IsRequired();
 
+    // Camera
     builder.Property(e => e.Camera).IsRequired().HasMaxLength(50);
 
+    // Location
     builder.Property(e => e.Location).IsRequired().HasMaxLength(50);
 
+    // Type
     builder.Property(e => e.Type).IsRequired().HasConversion<string>().HasMaxLength(20);
 
+    // ImageFile
     builder.Property(e => e.ImageFile).IsRequired().HasMaxLength(500);
 
+    // Description
     builder.Property(e => e.Description).IsRequired().HasMaxLength(1000);
   }
 }
